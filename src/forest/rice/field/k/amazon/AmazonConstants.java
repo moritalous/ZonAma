7d5fd97b2489cd7a8e098b8597ed1eea439d62c6
moritalous <moritalous@gmail.com>
2015-01-03 00:35:40 +0900

    Commit

---
+++ src/forest/rice/field/k/amazon/AmazonConstants.java
@@ -0,0 +1,18 @@
+package forest.rice.field.k.amazon;
+
+public class AmazonConstants {
+	
+	/*
+	 * Your AWS Access Key ID, as taken from the AWS Your Account page.
+	 */
+	public static final String AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID";
+
+	/*
+	 * Your AWS Secret Key corresponding to the above ID, as taken from the AWS
+	 * Your Account page.
+	 */
+	public static final String AWS_SECRET_KEY = "AWS_SECRET_KEY";
+
+	public static final String AWS_ASSOCIATE_TAG = "AWS_ASSOCIATE_TAG";
+
+}
