eab589e3b415d170c1bb9c3c45c2f5559de85929
moritalous <moritalous@gmail.com>
2015-01-06 23:25:04 +0900

    Commit

--- src/forest/rice/field/k/zonama/ui/MainActivity.java
+++ src/forest/rice/field/k/zonama/ui/MainActivity.java
@@ -2,16 +2,19 @@ package forest.rice.field.k.zonama.ui;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
 import forest.rice.field.k.zonama.R;
 
-public class MainActivity extends Activity implements OnQueryTextListener{
+public class MainActivity extends Activity implements OnQueryTextListener {
 
 	SearchView searchView = null;
-	
+
 	Fragment displayFragment = null;
 
 	@Override
@@ -23,31 +26,41 @@ public class MainActivity extends Activity implements OnQueryTextListener{
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.search_view, menu);
-
 		searchView = (SearchView) menu.findItem(R.id.searchView)
 				.getActionView();
-
 		searchView.setOnQueryTextListener(this);
+
+		MenuItem actionItem = menu.add("Source");
+		actionItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		Intent intent = new Intent(Intent.ACTION_VIEW,
+				Uri.parse("https://github.com/moritalous/ZonAma"));
+		startActivity(intent);
+		
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
 	public boolean onQueryTextSubmit(String query) {
-		if(query == null || query.equals("")) {
-			getFragmentManager().beginTransaction()
-			.remove(displayFragment)
-			.commit();
-			
+		if (query == null || query.equals("")) {
+			getFragmentManager().beginTransaction().remove(displayFragment)
+					.commit();
+
 			return true;
 		}
 		searchView.clearFocus();
-		
+
 		displayFragment = ItemListFragment.newInstance(query, "");
-		
-		 getFragmentManager().beginTransaction()
-		 .replace(R.id.container, displayFragment)
-		 .commit();
-		
+
+		getFragmentManager().beginTransaction()
+				.replace(R.id.container, displayFragment).commit();
+
 		return true;
 	}
 
